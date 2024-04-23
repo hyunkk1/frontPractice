8594d0a2723ca8dea8cdc6a5dd8aa378d4bca2d9
hyunkk1 <bbang970723@naver.com>
2024-04-23 15:00:07 +0900

    front practice git 연동

---
+++ JavaScript/js/나이계산.js
@@ -0,0 +1,9 @@
+let currentYear = 2024;
+let birthYear;
+let age;
+
+birthYear = prompt("태어난 연도를 입력 하세요 : ", "");
+age = currentYear - birthYear;
+document.write(currentYear + "년 현재 <br>");
+document.write(birthyear + "년에 태어날 사람의 나이는 " + age + "세 입니다.");
+
