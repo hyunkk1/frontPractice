47e8f66438067dbc80503b69230794e8ae036bc9
hyunkk1 <bbang970723@naver.com>
2024-04-25 13:40:14 +0900

    배경색 바꾸기

--- JavaScript/js/배경색변경.js
+++ JavaScript/js/배경색변경.js
@@ -1,9 +1,21 @@
-let colorBtn = document.getElementById("colorButton");
-const colors = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
-let colorIdx = 0;
+// jQuery 방식
 
-colorBtn.addEventListener("click", () => {
-  document.body.style.backgroundColor = colors[colorIdx];
-  colorIdx = (colorIdx + 1) % colors.length;
+// let colorBtn = document.getElementById("colorButton");
+// const colors = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
+// let colorIdx = 0;
+
+// colorBtn.addEventListener("click", () => {
+//   document.body.style.backgroundColor = colors[colorIdx];
+//   colorIdx = (colorIdx + 1) % colors.length;
+// });
+
+
+// JS 코드
+var colorButton = document.getElementById("colorButton");
+var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+var colorIndex = 0;
+
+colorButton.addEventListener("click", function() {
+  document.body.style.backgroundColor = colors[colorIndex];
+  colorIndex = (colorIndex + 1) % colors.length;
 });
-
