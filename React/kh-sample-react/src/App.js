c6e5b887a524f2317b1e81f9ebd7590619d2151a
hyunkk1 <bbang970723@naver.com>
2024-04-29 16:19:44 +0900

    React Props

--- React/kh-sample-react/src/App.js
+++ React/kh-sample-react/src/App.js
@@ -6,10 +6,31 @@ import "./App.css";
 // 조건부 연산자 : {}내에서 조건부 연산자 사용 가능
 // 조건부 렌더링 : && 연산자의 특징을 활용해서 조건에 따라 화면 랜더링
 
-import Greeting from "./Greeting";
+// import Greeting from "./Greeting";
+// import Welcome from "./Welcome";
+import Section from "./Section";
+import MyComponent from "./MyComponent";
 
+// 컴포넌트의 합성
 function App() {
-  return <Greeting />;
+  return (
+    <>
+      <Section
+        title="스포츠"
+        content="오늘의 프로야구는 기아가 1등 입니다."
+        imgUrl="./기아1등.png"
+        imgAlt="야구 이미지"
+      />
+      <Section title="정치" content="오늘은 영수 회담이 진행 중 입니다." />
+      <hr />
+      <MyComponent name="김" age={20} />
+      <MyComponent name="이" />
+      <MyComponent age={33} />
+      <MyComponent>
+        <h1>여기는 KH입니다.</h1>
+      </MyComponent>
+    </>
+  );
 }
 
-export default App;
+export default App; // export로 내보내기를 하지 않으면 못읽음. 쌍방으로 improt, export 걸어줘야함
